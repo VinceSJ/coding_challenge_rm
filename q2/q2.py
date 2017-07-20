017a92243b1b17859b3d6ec9fa1fe95af099ced0
VinceSJ <vincesj@gmail.com>
2017-07-20 16:35:23 -0700

    Made a Smart Alec version for yucks.

---
+++ q2/q2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# Should work fine with python2 as well, but testing and development was done in 3.
+
+# Given a list of strings containing only values "fizz", "buzz" or "fizzbuzz", determine if there exists a sequence
+# of numbers that will return the given string when run through the function from Q1. If such a sequence exists,
+# determine the shortest such sequence. - If no such sequence exists, return an empty list: [] - If the sequence
+# exists, return a list that is the shortest sequence: e.g., [9, 10] for an input of ["fizz", "buzz"]. - If multiple
+# shortest sequences exist (consider that ["buzz", "fizz"] can be created at shortest length of 2 with both [5,
+# 6] and [20, 21]), return the sequence with the lower starting number: e.g., in this case we would return [5,
+# 6] for ["buzz", "fizz"]
+
+
+wordlist = ['fizz', 'buzz']
+
+
+
